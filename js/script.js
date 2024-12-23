b9ffd5f7d37ae9ea1f34f55bff7c10351b93d722
SadmanMuhit <sadmanmuhit@gmail.com>
2024-12-24 01:34:07 +0600

    innovation porject

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$('.counterUp').counterUp({
+    delay: 10,
+    time: 1000,
+});
+$('.sarvices_body').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: false,
+    prevArrow: false
+  });
+  $('.feedback_body').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: false,
+    prevArrow: false
+  });
